296a61aa774826d3c6a4bb64fd248360b0e872fb
raywang <625524105@qq.com>
2017-06-24 14:10:20 +0800

    another commit

--- 001.sh
+++ 001.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Date: `date`"
 echo "Initiating scann..."
+set -x
 nmap -sP 192.168.1.1/24 | grep report | awk '{print $5}' -> 001output.txt
+set +x
 echo "Commands complete."
---
+++ positional.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "positional parameter shell script."
+POSPAR1="$1"
+POSPAR2="$2"
+echo "$1 is the first parameter and $2 the second parameter."
+
+echo "Total number of positional parameter is $#"
